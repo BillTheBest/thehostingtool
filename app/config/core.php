32d4d302788ee49bfd9df0a4d58fcf78a644a5ec
kmark <kmark937@gmail.com>
2010-07-24 01:56:13 +0000

    Made Cake console commands executable and added a comment to core.php
    git-svn-id: http://thehostingtool.googlecode.com/svn/branches/2.0@122 3066ca77-9623-52e6-b9fb-6191fef3a309

--- app/config/core.php
+++ app/config/core.php
@@ -1,4 +1,7 @@
 <?php
+
+// If someone told you to edit this file, they probably meant core.private.php
+
 if(file_exists(CONFIGS . 'core.private.php')) {
 	require(CONFIGS . 'core.private.php');
 }
